c0116e58b5d035430e119477fe398a0633827642
Ove Nystås <ove.nystas@gmail.com>
2020-02-05 00:17:36 +0100

    fix: Add custom hass devices for some Fibaro sensors #233
    Fibaro FGMS001 Motion Sensor
    Fibaro FGMS001-ZW5 Motion Sensor
    Fibrao FGSS101 Smoke Sensor
    Fibrao FGFS101 Flood Sensor

--- hass/devices.js
+++ hass/devices.js
@@ -101,8 +101,122 @@ module.exports = {
       }
     }
   ],
+  '271-4097-2048': [ // Fibaro Motion Sensor FGMS001
+    { // General Purpose Alarm
+      type: 'binary_sensor',
+      object_id: 'alarm',
+      values: ['156-1-0'],
+      discovery_payload: {
+        payload_on: 255,
+        payload_off: 0,
+        value_template: '{{ value_json.value }}',
+        device_class: 'problem'
+      }
+    },
+    { // Motion Sensor
+      type: 'binary_sensor',
+      object_id: 'motion',
+      values: ['48-1-0'],
+      discovery_payload: {
+        payload_on: true,
+        payload_off: false,
+        value_template: '{{ value_json.value }}',
+        device_class: 'motion'
+      }
+    },
+  ],
+  '271-4098-2049': [ // Fibaro Motion Sensor FGMS001-ZW5
+    { // Binary Sensor, General Purpose Alarm
+      type: 'binary_sensor',
+      object_id: 'alarm',
+      values: ['156-1-0'],
+      discovery_payload: {
+        payload_on: 255,
+        payload_off: 0,
+        value_template: '{{ value_json.value }}',
+        device_class: 'problem'
+      }
+    },
+    { // Binary Sensor, Motion Sensor
+      type: 'binary_sensor',
+      object_id: 'motion',
+      values: ['48-1-0'],
+      discovery_payload: {
+        payload_on: true,
+        payload_off: false,
+        value_template: '{{ value_json.value }}',
+        device_class: 'motion'
+      }
+    },
+    { // Multilevel Sensor, Seismic intensity
+      type: 'sensor',
+      object_id: 'seismic_intensity',
+      values: ['49-1-25'],
+      discovery_payload: {
+        unit_of_measurement: 'mm',
+        value_template: '{{ value_json.value }}',
+        icon: 'mdi:pulse'
+      }
+    },
+    { // Multilevel Sensor, Acceleration, X-axis
+      type: 'sensor',
+      object_id: 'acceleration_x',
+      values: ['49-1-52'],
+      discovery_payload: {
+        unit_of_measurement: 'm/s²',
+        value_template: '{{ value_json.value }}',
+        icon: 'mdi:axis-x-arrow'
+      }
+    },
+    { // Multilevel Sensor, Acceleration, Y-axis
+      type: 'sensor',
+      object_id: 'acceleration_y',
+      values: ['49-1-53'],
+      discovery_payload: {
+        unit_of_measurement: 'm/s²',
+        value_template: '{{ value_json.value }}',
+        icon: 'mdi:axis-y-arrow'
+      }
+    },
+    { // Multilevel Sensor, Acceleration, Z-axis
+      type: 'sensor',
+      object_id: 'acceleration_z',
+      values: ['49-1-54'],
+      discovery_payload: {
+        unit_of_measurement: 'm/s²',
+        value_template: '{{ value_json.value }}',
+        icon: 'mdi:axis-z-arrow'
+      }
+    },
+  ],
   '99-12340-18756': [FAN_DIMMER], // GE 1724 Dimmer
   '99-12593-18756': [FAN_DIMMER], // GE 1724 Dimmer
   '328-1-3': [SPIRIT_ZWAVE_PLUS],
   '328-3-3': [SPIRIT_ZWAVE_PLUS]
 }
+// x-y-z
+/* Command classes x-
+48  0x30 Binary Sensor
+49  0x31 Multilevel Sensor
+64  0x40 Thermostat Mode
+67  0x43 Thermostat Setpoint
+128 0x80 Battery
+156 0x9C Alarm Sensor
+*/
+/* xxx -y-
+*/
+/* Binary Sensor Types -z
+0   0x00 General Purpose Alarm
+1   0x01 Smoke Alarm
+2   0x02 CO Alarm
+3   0x03 CO2 Alarm
+4   0x04 Heat Alarm
+5   0x05 Water Leak Alarm
+255 0xFF Return first Alarm on supported list
+*/
+/* Multilevel Sensor Types -z
+25  0x19 Seismic Intensity
+52  0x34 Acceleration, X-axis
+53  0x35 Acceleration, Y-axis
+54  0x36 Acceleration, Z-axis
+*/
